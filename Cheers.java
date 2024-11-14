0162c3b1aa36f5d6346119298bb9d8e3cb34912b
tomer1408 <tomer1404r@gmail.com>
2024-11-14 21:26:47 +0200

    homeWork4

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,38 @@
-// Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+            String name = args[0];
+            int numerOfTimes = Integer.parseInt(args[1]);
+            int nameLength = name.length();
+            int i = 0;
+            int y = 0;
+    
+            char[] charsToFind = {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'};
+    
+            while (i < nameLength) {
+                char letter = name.charAt(i);
+                boolean startsWithVowel = false;
+    
+                for (char c : charsToFind) {
+                    if (Character.toUpperCase(letter) == c) {
+                        startsWithVowel = true;
+                        break;
+                    }
+                }
+    
+                if (startsWithVowel) {
+                    System.out.println("Give me an " + letter + ": " + letter + "!");
+                } else {
+                    System.out.println("Give me a " + letter + ": " + letter + "!");
+                }
+    
+                i++;
+            }
+    
+            System.out.println("What does that spell?"); 
+            while (y < numerOfTimes) {  
+                System.out.println(name + "!");
+                y++;
+            }
         }
-}
+    }
+    
