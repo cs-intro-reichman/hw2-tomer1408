e01ae6705bd4b7e9bf6af17a7db89a211666a1b1
tomer1408 <tomer1404r@gmail.com>
2024-11-14 21:47:05 +0200

    homeWork2

--- Cheers.java
+++ Cheers.java
@@ -7,19 +7,18 @@ public class Cheers {
             int y = 0;
     
             char[] charsToFind = {'A', 'E', 'F', 'H', 'I', 'L', 'M', 'N', 'O', 'R', 'S', 'X'};
-            boolean useAn = false;
-    
-            char firstLetter = name.charAt(0);
-            for (char c : charsToFind) {
-                if (Character.toUpperCase(firstLetter) == c) {
-                    useAn = true;
-                    break;
-                }
-            }
     
             while (i < nameLength) {
                 char letter = name.charAt(i);
-                letter = Character.toUpperCase(letter); 
+                boolean useAn = false;
+    
+                for (char c : charsToFind) {
+                    if (Character.toUpperCase(letter) == c) {
+                        useAn = true;
+                        break;
+                    }
+                }
+    
                 if (useAn) {
                     System.out.println("Give me an " + letter + ": " + letter + "!");
                 } else {
