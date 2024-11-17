ef438da002d570698e22f4752febda84378e710d
tomer1408 <tomer1404r@gmail.com>
2024-11-17 19:28:32 +0200

    homeWork2

--- CalcPi.java
+++ CalcPi.java
@@ -16,7 +16,7 @@ public class CalcPi {
         }
 
         piApproximated *= 4; 
-        System.out.println("pi according to Java: " + Math.PI);
-        System.out.println("pi, approximated: " + piApproximated);
+		System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + piApproximated);
     }
 }
